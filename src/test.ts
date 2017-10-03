9ed7627de79ec6df632487844af1e564c12e22f9
Chris Feijoo <christophe.feijoo@teads.tv>
2017-10-03 14:31:40 +0200

    Prettier on code

--- src/test.ts
+++ src/test.ts
@@ -12,96 +12,93 @@ import 'mocha'
 import { expect } from 'chai'
 import when from './when'
 
-describe('when', () => {
+// Here getDrinkPrice returns a number
+describe('with a simple return-type', () => {
+  const getDrinkPrice = (drink: 'Pepsi' | 'Coke' | 'Orangina'): number =>
+    when(drink)
+      .is('Coke', 1.5)
+      .is('Pepsi', 1.8)
+      .else(2.0)
 
-  // Here getDrinkPrice returns a number
-  describe('with a simple return-type', () => {
-    const getDrinkPrice =
-      (drink: 'Pepsi' | 'Coke' | 'Orangina'): number =>
-        when(drink)
-          .is('Coke', 1.50)
-          .is('Pepsi', 1.80)
-          .else(2.00)
-
-    it('returns value if matches an expression', () => {
-      expect(getDrinkPrice('Coke')).to.equal(1.50)
-      expect(getDrinkPrice('Pepsi')).to.equal(1.80)
-    })
-
-    it('returns default value if no match', () => {
-      expect(getDrinkPrice('Orangina')).to.equal(2.00)
-    })
+  it('returns value if matches an expression', () => {
+    expect(getDrinkPrice('Coke')).to.equal(1.5)
+    expect(getDrinkPrice('Pepsi')).to.equal(1.8)
   })
 
-  // Here getDrinkPrice can return number, string or boolean
-  // We expect TypeScript to not throw any type error
-  describe('with a union return-type', () => {
-    const getDrinkPrice =
-      (drink: 'Pepsi' | 'Coke' | 'Orangina'): number | string | boolean =>
-        when(drink)
-          .is('Coke', 1.50)
-          .is('Pepsi', true)
-          .else('Free')
+  it('returns default value if no match', () => {
+    expect(getDrinkPrice('Orangina')).to.equal(2.0)
+  })
+})
 
-    it('returns value if matches an expression', () => {
-      expect(getDrinkPrice('Coke')).to.equal(1.50)
-      expect(getDrinkPrice('Pepsi')).to.equal(true)
-    })
+// Here getDrinkPrice can return number, string or boolean
+// We expect TypeScript to not throw any type error
+describe('with a union return-type', () => {
+  const getDrinkPrice = (
+    drink: 'Pepsi' | 'Coke' | 'Orangina'
+  ): number | string | boolean =>
+    when(drink)
+      .is('Coke', 1.5)
+      .is('Pepsi', true)
+      .else('Free')
 
-    it('returns default value if no match', () => {
-      expect(getDrinkPrice('Orangina')).to.equal('Free')
-    })
+  it('returns value if matches an expression', () => {
+    expect(getDrinkPrice('Coke')).to.equal(1.5)
+    expect(getDrinkPrice('Pepsi')).to.equal(true)
   })
 
-  describe('with a function as `is` return value', () => {
-    type Action = { type: string }
-    const action: Action = { type: 'INCREMENT' }
+  it('returns default value if no match', () => {
+    expect(getDrinkPrice('Orangina')).to.equal('Free')
+  })
+})
 
-    // We expect the return type of apply to be `string | number | boolean`
-    const apply = (action: Action) =>
-      when(action.type)
-        .is('INCREMENT', () => 2)
-        .is('DECREMENT', () => true)
-        .else('NONE')
+describe('with a function as `is` return value', () => {
+  type Action = { type: string }
+  const action: Action = { type: 'INCREMENT' }
 
-    it('returns value if matches an expression', () => {
-      expect(apply({ type: 'INCREMENT' })).to.equal(2)
-      expect(apply({ type: 'DECREMENT' })).to.equal(true)
-    })
+  // We expect the return type of apply to be `string | number | boolean`
+  const apply = (action: Action) =>
+    when(action.type)
+      .is('INCREMENT', () => 2)
+      .is('DECREMENT', () => true)
+      .else('NONE')
 
-    it('returns default value if no match', () => {
-      expect(apply({ type: 'Hello' })).to.equal('NONE')
-      expect(apply({ type: 'World' })).to.equal('NONE')
-    })
+  it('returns value if matches an expression', () => {
+    expect(apply({ type: 'INCREMENT' })).to.equal(2)
+    expect(apply({ type: 'DECREMENT' })).to.equal(true)
   })
 
-  describe('when entry expression is a string', () => {
-    it('provides a `match` method', () => {
-      expect(when('hello').match).to.be.a('function')
-    })
+  it('returns default value if no match', () => {
+    expect(apply({ type: 'Hello' })).to.equal('NONE')
+    expect(apply({ type: 'World' })).to.equal('NONE')
+  })
+})
 
-    describe('match method', () => {
-      const getCaseStyle = (text: string) =>
-        when(text)
-          .match(/^([A-Z][a-z]*)+$/, 'UpperCamelCase')
-          .match(/^([a-z]+[A-Z][a-z]*)+$/, 'LowerCamelCase')
-          .match(/^([a-z]+_[a-z]+)+$/, 'SnakeCase')
-          .else('Unknown')
-
-      it('returns first match', () => {
-        expect(getCaseStyle('Hello')).to.equal('UpperCamelCase')
-        expect(getCaseStyle('HelloWorld')).to.equal('UpperCamelCase')
-        expect(getCaseStyle('helloWorld')).to.equal('LowerCamelCase')
-        expect(getCaseStyle('hello_world')).to.equal('SnakeCase')
-        expect(getCaseStyle('hello')).to.equal('Unknown')
-        expect(getCaseStyle('Hello_World')).to.equal('Unknown')
-      })
-    })
+describe('when entry expression is a string', () => {
+  it('provides a `match` method', () => {
+    expect(when('hello').match).to.be.a('function')
   })
 
-  describe('when entry expression is not a string', () => {
-    it('does not provides a `match` method', () => {
-      expect(when(42).match).to.be.undefined
+  describe('match method', () => {
+    const getCaseStyle = (text: string) =>
+      when(text)
+        .match(/^([A-Z][a-z]*)+$/, 'UpperCamelCase')
+        .match(/^([a-z]+[A-Z][a-z]*)+$/, 'LowerCamelCase')
+        .match(/^([a-z]+_[a-z]+)+$/, 'SnakeCase')
+        .else('Unknown')
+
+    it('returns first match', () => {
+      expect(getCaseStyle('Hello')).to.equal('UpperCamelCase')
+      expect(getCaseStyle('HelloWorld')).to.equal('UpperCamelCase')
+      expect(getCaseStyle('helloWorld')).to.equal('LowerCamelCase')
+      expect(getCaseStyle('hello_world')).to.equal('SnakeCase')
+      expect(getCaseStyle('hello')).to.equal('Unknown')
+      expect(getCaseStyle('Hello_World')).to.equal('Unknown')
     })
+  })
+})
+
+describe('when entry expression is not a string', () => {
+  it('does not provides a `match` method', () => {
+    expect(when(42).match).to.be.undefined
   })
 })
--- src/when.ts
+++ src/when.ts
@@ -9,49 +9,33 @@
       ## ## ##*/
 
 export type When<T, V> = {
-  is:
-  <I>(expr: T, value: ((expr?: T) => I) | I) => When<T, V | I>,
-
-  match:
-  <I>(expr: RegExp, value: ((expr?: T) => I) | I) => When<T, V | I>,
-
-  else:
-  <E>(value: ((expr?: T) => E) | E) => V | E
+  is: <I>(expr: T, value: ((expr?: T) => I) | I) => When<T, V | I>
+  match: <I>(expr: RegExp, value: ((expr?: T) => I) | I) => When<T, V | I>
+  else: <E>(value: ((expr?: T) => E) | E) => V | E
 }
 
 const resolvedWhen = <T, V>(resolvedValue: V) => ({
-  is: () =>
-    resolvedWhen(resolvedValue),
-
-  match: () =>
-    resolvedWhen(resolvedValue),
-
-  else: () =>
-    resolvedValue
+  is: () => resolvedWhen(resolvedValue),
+  match: () => resolvedWhen(resolvedValue),
+  else: () => resolvedValue
 })
 
 const when = <T>(expr: T) => ({
-  is:
-  <V>(constExpr: T, value: ((expr?: T) => V) | V): When<T, V> =>
-    expr === constExpr ?
-      resolvedWhen(
-        value instanceof Function ? value(expr) : value
-      )
+  is: <V>(constExpr: T, value: ((expr?: T) => V) | V): When<T, V> =>
+    expr === constExpr
+      ? resolvedWhen(value instanceof Function ? value(expr) : value)
       : when(expr),
 
   // Provide `match` method only if `expr` is a string
   match:
-  typeof expr === 'string' ?
-    <V>(regExp: RegExp, value: ((expr?: T) => V) | V): When<T, V> =>
-      regExp.test(expr) ?
-        resolvedWhen(
-          value instanceof Function ? value(expr) : value
-        )
-        : when(expr)
-    : undefined,
+    typeof expr === 'string'
+      ? <V>(regExp: RegExp, value: ((expr?: T) => V) | V): When<T, V> =>
+          regExp.test(expr)
+            ? resolvedWhen(value instanceof Function ? value(expr) : value)
+            : when(expr)
+      : undefined,
 
-  else:
-  <V>(defaultValue: ((_: T) => V) | V): V =>
+  else: <V>(defaultValue: ((_: T) => V) | V): V =>
     defaultValue instanceof Function ? defaultValue(expr) : defaultValue
 })
 
