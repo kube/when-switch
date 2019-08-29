adc3fae71ef17b0f985c98f6d859a18da40f0d7d
nawordar <czarek@drozak.net>
2019-08-29 21:47:50 +0200

    Add true method

--- src/when.ts
+++ src/when.ts
@@ -23,13 +23,19 @@ export type When<T, V> = {
     returnValue: ((inputValue: U) => W) | W
   ) => When<T, V | W>
 
-  else: <W>(returnValue: Callable<T, W> | W) => V | W
+  else: <W>(returnValue: ((inputValue: T) => W) | W) => V | W
 }
 
-export type Callable<T, V> = (inputValue: T) => V
+export interface WhenSwitch {
+  <T>(expr: T): When<T, never>
+  
+  /** Tests assertion and returns _value_ if assertion is true. */
+  true: <T>(assertion: (() => boolean) | boolean, value: (() => T) | T) => True<T>
+}
 
-function isCallable<T, V>(inputValue: any): inputValue is Callable<T, V> {
-  return typeof inputValue === 'function'
+export type True<T> = {
+  true: <U>(assertion: (() => boolean) | boolean, value: (() => U) | U) => True<T | U>,
+  else: <U>(returnValue: (() => U) | U) => T | U
 }
 
 /**
@@ -45,19 +51,39 @@ const resolve = (resolvedValue: any): When<any, any> => ({
 /**
  * Tests an object against multiple expressions.
  */
-export const when = <T>(expr: T): When<T, never> => ({
+export const when = (<T>(expr: T): When<T, never> => ({
   is: (constExpr, value) =>
     expr === constExpr
-      ? resolve(isCallable(value) ? value(constExpr) : value)
+      ? resolve(typeof value === 'function' ? (value as (x: any) => any)(constExpr) : value)
       : when(expr),
 
   match: (matcher, value) =>
     matcher.test(expr)
-      ? resolve(isCallable(value) ? value(expr) : value)
+      ? resolve(typeof value === 'function' ? (value as (x: any) => any)(expr) : value)
       : when(expr),
 
   else: defaultValue =>
-    isCallable<T, any>(defaultValue) ? defaultValue(expr) : defaultValue
+    typeof defaultValue === 'function' ? (defaultValue as (x: any) => any)(expr) : defaultValue
+})) as WhenSwitch
+
+/**
+ * Exposes same API as `true`, but just propagates a resolved value,
+ * without doing any further test.
+ */
+const resolveAssertion = (resolvedValue: any): True<any> => ({
+  true: () => resolveAssertion(resolvedValue),
+  else: () => resolvedValue
 })
+
+when.true = <T>(assertion: (() => boolean) | boolean, value: (() => T) | T): True<T> =>
+  (typeof assertion === 'function' ? assertion() : assertion)
+    ? resolveAssertion(typeof value === 'function' ? (value as (() => any))() : value)
+    : ({
+      true: when.true,
+      else: defaultValue =>
+        typeof defaultValue === 'function'
+          ? (defaultValue as (() => any))()
+          : defaultValue
+    })
 
 export default when
